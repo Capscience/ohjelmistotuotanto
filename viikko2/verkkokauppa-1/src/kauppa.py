9d7f3a9733d62fa61959376c2ba9db8c076408fd
Simo Soini <simo@capscience.fi>
2022-11-14 19:56:02 +0200

    viikko2 tehtävät 10-12

--- viikko2/verkkokauppa-1/src/kauppa.py
+++ viikko2/verkkokauppa-1/src/kauppa.py
@@ -1,14 +1,16 @@
-from varasto import Varasto
-from pankki import Pankki
+from varasto import the_varasto_olio
+from pankki import the_pankki_olio
 from ostoskori import Ostoskori
-from viitegeneraattori import Viitegeneraattori
+from viitegeneraattori import the_viitegeneraattori_olio
 
 
 class Kauppa:
-    def __init__(self):
-        self._varasto = Varasto.get_instance()
-        self._pankki = Pankki.get_instance()
-        self._viitegeneraattori = Viitegeneraattori.get_instance()
+    def __init__(self, varasto=the_varasto_olio,
+                 pankki=the_pankki_olio,
+                 viitegeneraattori=the_viitegeneraattori_olio):
+        self._varasto = varasto
+        self._pankki = pankki
+        self._viitegeneraattori = viitegeneraattori
         self._kaupan_tili = "33333-44455"
 
     def aloita_asiointi(self):
@@ -29,4 +31,5 @@ class Kauppa:
         viite = self._viitegeneraattori.uusi()
         summa = self._ostoskori.hinta()
 
-        return self._pankki.tilisiirto(nimi, viite, tili_numero, self._kaupan_tili, summa)
+        return self._pankki.tilisiirto(nimi, viite, tili_numero,
+                                       self._kaupan_tili, summa)
